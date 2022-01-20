5037d6bbd06e3904fadb8af23a9c53d0eac28a70
Dmitry Dudin <dmitrodonskoy@yandex.ru>
2022-01-20 22:36:38 +0300

    Разработка лендинга

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,11 +6,11 @@ function Footer() {
         <footer className="footer">
             <h4 className="footer__tittle">Учебный проект Яндекс.Практикум х BeatFilm.</h4>
             <div className="footer__container">
-                <p>&#169;{year}</p>
+                <p>&#169; {year}</p>
                 <ul className="footer__list">
-                    <li className="footer__item"><a>Яндекс.Практикум</a></li>
-                    <li className="footer__item"><a>Github</a></li>
-                    <li className="footer__item"><a>Facebook</a></li>
+                    <li className="footer__item"><a className="footer__link" href="https://practicum.yandex.ru" target="_blank" rel="noreferrer">Яндекс.Практикум</a></li>
+                    <li className="footer__item"><a className="footer__link" href="https://github.com" target="_blank" rel="noreferrer">Github</a></li>
+                    <li className="footer__item"><a className="footer__link" href="https://www.facebook.com" target="_blank" rel="noreferrer">Facebook</a></li>
                 </ul>
             </div>
         </footer>
--- src/components/Main/AboutMe/AboutMe.js
+++ src/components/Main/AboutMe/AboutMe.js
@@ -7,17 +7,17 @@ function AboutMe() {
             <div className="lead__tittle-container">
                 <h2 className="lead__tittle">Студент</h2>
             </div>
-            <ul className="lead__content-container lead__content-container_about-me" style={{}}>
+            <ul className="lead__content-container lead__content-container_about-me">
                 <li className="lead__content-item lead__content-item_about-me-text">
-                    <h3 className="lead__item-tittle lead__item-tittle_about-me">Виталий</h3>
+                    <h3 className="lead__item-tittle lead__item-tittle_about-me">Дмитрий</h3>
                     <p className="lead__item-text lead__item-text_about-me">Фронтенд-разработчик, 25 лет</p>
-                    <p className="lead__item-text" style={{lineHeight: "22px"}}>Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена
+                    <p className="lead__item-text" style={{ lineHeight: "22px" }}>Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена
                         и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в компании
                         «СКБ Контур». После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.</p>
-                        <ul className='lead__navbar'>
-                            <li><a >Facebook</a></li>
-                            <li><a >Github</a></li>
-                        </ul>
+                    <ul className='lead__navbar'>
+                        <li><a className='lead__navlink' href="https://www.facebook.com" target="_blank" rel="noreferrer">Facebook</a></li>
+                        <li><a className='lead__navlink' href="https://github.com/Thanatas42" target="_blank" rel="noreferrer">Github</a></li>
+                    </ul>
                 </li>
                 <li className="lead__content-item lead__content-item_about-me-photo">
                     <img src={avatar} alt="альтернативный текст" />

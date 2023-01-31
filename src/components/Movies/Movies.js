db96e04cc15b3dfa00fa28609d20fc5b1354e109
Dmitry Dudin <dmitrodonskoy@yandex.ru>
2023-01-31 12:38:56 +0300

    Рефакторинг компонентов movies

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,19 +1,13 @@
 import React from 'react';
-import { useContext, useState, useEffect } from "react";
-import * as MoviesApi from '../../utils/MoviesApi'
 import MoviesCardList from './MoviesCardList/MoviesCardList';
-import resizeEvent from '../../utils/ResizeEvent';
-import { DurationShortFilms } from '../../utils/constants';
 import Preloader from '../../components/Preloader/Preloader';
 
 function Movies(props) {
-    const [inSearch, setInSearch] = useState(localStorage.getItem('searchInput') ? true : false);
-    const [inSearchString, setInSearchString] = useState(localStorage.getItem('searchInput') ? localStorage.getItem('searchInput') : '');
 
     return (
         <>
             {props.resStatus ? <Preloader /> :
-                <MoviesCardList moviesArray={props.moviesArray} deleteMovies={props.deleteMovies} likedMovies={props.likedMovies} />
+                <MoviesCardList {...props} />
             }
             {!props.resStatus && props.moviesArray.length === 0 ? <p className="cards__empty">Во время запроса произошла ошибка. Возможно, проблема
                 с соединением или сервер недоступен. Подождите немного и попробуйте ещё раз</p> : <></>}
